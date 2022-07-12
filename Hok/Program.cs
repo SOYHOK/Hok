a298d9cfdaec609ba7d61b24ff4bd18638d803d4
soy hok <soyhok2018@gmail.com>
2022-07-12 17:30:20 +0700

    Add project files.

---
+++ Hok/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int FirstInterger;
+            int SecondInterger;
+
+            Console.WriteLine("This program adds for you two intergers");
+            Console.Write("Type the 1st interger: ");
+            FirstInterger = int.Parse(Console.ReadLine());
+
+            Console.Write("Type the 2nd interger: ");
+            SecondInterger = int.Parse(Console.ReadLine()); 
+
+            Console.WriteLine((FirstInterger + SecondInterger));
+
+
+        }
+    }
+}
